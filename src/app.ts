43496dba49c94efd80deff6ea11d2b832b7e3bab
Guilherme Zuriel <guizuriel.dev@gmail.com>
2024-03-26 11:54:52 -0300

    Adicionando @prisma/client

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,14 @@
 import fastify from 'fastify';
+import { PrismaClient } from '@prisma/client';
 
 const app = fastify();
+const prisma = new PrismaClient();
+
+prisma.user.create({
+  data: {
+    name: 'Guilherme Zuriel',
+    email: 'guizuriel@email.com',
+  },
+});
 
 export default app;
