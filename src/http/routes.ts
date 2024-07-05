6b9a34426c867417ffc3213ac3af2a71bc5da567
Guilherme Zuriel <guizuriel.dev@gmail.com>
2024-07-05 14:07:59 -0300

    Controller de registro

---
+++ src/http/routes.ts
@@ -0,0 +1,6 @@
+import fastify, { FastifyInstance } from 'fastify';
+import { register } from './controllers/register.controller';
+
+export async function appRoutes(app: FastifyInstance) {
+  app.post('/users', register);
+}
