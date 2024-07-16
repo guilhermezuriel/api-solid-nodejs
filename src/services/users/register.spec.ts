395825094dbb8d0d1e6adc383cc15d343b463f81
Guilherme Zuriel <guizuriel.dev@gmail.com>
2024-07-16 19:59:17 -0300

    Coverage de testes

--- src/services/users/register.spec.ts
+++ src/services/users/register.spec.ts
@@ -49,7 +49,7 @@ describe('Register User Case', () => {
       password: '123456',
     });
 
-    expect(() =>
+    await expect(() =>
       registerUseCase.execute({
         name: 'John Node',
         email,
